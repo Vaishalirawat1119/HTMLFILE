ae88f07df8d66b32bc6cbcf91838a42eacaf8209
Vaishalirawat1119 <112968518+Vaishalirawat1119@users.noreply.github.com>
2024-01-16 21:49:56 +0530

    Taking number as input from user and performing some basic operations on array.

--- JavaScript/myfile.js
+++ JavaScript/myfile.js
@@ -1,14 +1,52 @@
-let company = ["Bloomberg" , "Microsoft" , "Uber" , "Google" , "IBM" , "Netflix"];
-console.log(company);
+let num = prompt("Enter any number");
+let arr = [];
 
-//Remove first company (Bloomberg) from the array.
-let remove = company.shift();
-console.log(remove);
+for(let i=1; i<=num; i++){
+    arr[i-1] = i;
+}
 
-//Remove Uber and add OLA in its place.
-let delAndAdd = company.splice(1,1,"OLA");
-console.log(delAndAdd);
+let sum = arr.reduce((prev,cur) =>{
+    return prev+cur;
+});
+console.log(`Sum: ${sum}`);
 
-//Add Amazon in the end.
-let addCompany = company.push("Amazon");
-console.log(addCompany);
+let fact = arr.reduce((prev,cur) =>{
+    return prev*cur;
+});
+console.log(`Factorial: ${fact}`);
+
+// let stuMarks = [56,73,81,34,85,90,94,99];
+// const res = stuMarks.filter((stuMarks) =>{
+//     return stuMarks>=90;
+// });
+
+// console.log(res);
+
+
+
+// function vowelsCheck(word){
+//     let count = 0;
+//     for(const char of word){
+//         if(char === "a" ||char === "e" ||char === "i" ||char === "o" ||char === "u"){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+
+
+// let company = ["Bloomberg" , "Microsoft" , "Uber" , "Google" , "IBM" , "Netflix"];
+// console.log(company);
+
+// //Remove first company (Bloomberg) from the array.
+// let remove = company.shift();
+// console.log(remove);
+
+// //Remove Uber and add OLA in its place.
+// let delAndAdd = company.splice(1,1,"OLA");
+// console.log(delAndAdd);
+
+// //Add Amazon in the end.
+// let addCompany = company.push("Amazon");
+// console.log(addCompany);
